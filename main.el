3c1589bb8b6a2a45102baf22628f69b37b8d2533
Joseph Abrahamson <abrahamson.j@gmail.com>
2010-03-17 01:54:08 -0400

    * main.el: Installed ELPA to main.el file

--- main.el
+++ main.el
@@ -115,6 +115,17 @@
 ;;
 (setq inhibit-splash-screen t)
 
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
+
 ;; Welcome to Emacs.
 ;;
 (require 'shell-start)
